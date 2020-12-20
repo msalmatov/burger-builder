76608a85e0819a95d569665415acb457131ec8a0
mroot <almatov.ms@gmail.com>
2020-12-20 22:49:34 +0500

    BurgerIngredient, Burger

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import styles from './Layout.module.css';
 
 const layout = (props) => {
     return (
         <Aux>
             <div>ToolBar, SideDrawer, Backdrop</div>
-            <main>
+            <main className={styles.Content}>
                 {props.children}
             </main>
         </Aux>

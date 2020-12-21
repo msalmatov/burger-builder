fc887a309c7be87ba2d44e8fa0cb82c55684521c
mroot <almatov.ms@gmail.com>
2020-12-21 14:47:10 +0500

    add/remove burger ingredients

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+];
+
+const buildControls = (props) => (
+    <div className={styles.BuildControls}>
+        <p>Current price: {props.price.toFixed(2)}</p>
+        {controls.map(ctrl => (
+                <BuildControl 
+                    key={ctrl.label} 
+                    label={ctrl.label}
+                    added={() => props.ingredientAdded(ctrl.type)}
+                    removed={() => props.inredientRemoved(ctrl.type)}
+                    disabled={props.disabled[ctrl.type]} />
+        ))}
+    </div>
+);
+
+export default buildControls;

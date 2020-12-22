fdc7779975fdf13b762884808546f95ba112d252
mroot <almatov.ms@gmail.com>
2020-12-22 16:32:55 +0500

    add SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 import styles from './Layout.module.css';
 
-const layout = (props) => {
-    return (
-        <Aux>
-            <div>ToolBar, SideDrawer, Backdrop</div>
-            <main className={styles.Content}>
-                {props.children}
-            </main>
-        </Aux>
-    );
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    };
+
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={styles.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+        }
 };
 
-export default layout;
+export default Layout;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Logo.module.css';
+import burgerLogo from '../../assets/images/burger-logo.png';
+
+const Logo = (props) => (
+    <div 
+        className={styles.Logo}
+        style={{height: props.height}} >
+        <img src={burgerLogo} alt='MyBurger' />
+    </div>
+);
+
+export default Logo;

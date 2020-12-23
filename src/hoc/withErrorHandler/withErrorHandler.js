311886168d70dee78d53985ebf3eb67edba4f3ff
mroot <almatov.ms@gmail.com>
2020-12-23 16:35:56 +0500

    http request

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -4,21 +4,23 @@ import Aux from '../Aux/Aux';
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
-        state = {
-            error: null
-        }
+        constructor(props) {
+            super(props);
+            
+            this.state = {
+                error: null
+            };
 
-        componentDidMount() {
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState( { error: null } );
                 return req;
             });
 
-            axios.interceptors.response.use(resp => resp, error => {
+            this.respInterceptor = axios.interceptors.response.use(resp => resp, error => {
                 this.setState( { error: error } );
             });
         }
-        
+
         errorConfirmedHandler = () => {
             this.setState( { error: null } );
         }
@@ -35,6 +37,12 @@ const withErrorHandler = (WrappedComponent, axios) => {
                 </Aux>
             );
         }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.respInterceptor);
+        }
+        
     }
 };
 

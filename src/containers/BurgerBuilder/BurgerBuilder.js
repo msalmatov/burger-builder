66ca669ea165677c618c9aef88db769f084bc265
mroot <almatov.ms@gmail.com>
2020-12-24 16:54:44 +0500

    Roting related changes

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -88,31 +88,43 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState( { loading: true } );
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Murod Almatov',
-                address: {
-                    street: 'test street 1',
-                    zipCode: '123412341234',
-                    country: 'Uzbekistan'
-                },
-                email: 'almatov.ms@gmail.com'
-            },
-            deliveryMethod: 'fastest'
-        };
+        // this.setState( { loading: true } );
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Murod Almatov',
+        //         address: {
+        //             street: 'test street 1',
+        //             zipCode: '123412341234',
+        //             country: 'Uzbekistan'
+        //         },
+        //         email: 'almatov.ms@gmail.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // };
 
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState( { loading: false, purchasing: false } );
-                console.log(response);
-            })
-            .catch(error => {
-                this.setState( { loading: false, purchasing: false } );
-                console.log(error);
-            });
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState( { loading: false, purchasing: false } );
+        //         console.log(response);
+        //     })
+        //     .catch(error => {
+        //         this.setState( { loading: false, purchasing: false } );
+        //         console.log(error);
+        //     });
+
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+
+        const queryString = queryParams.join('&');
+
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
     render() {

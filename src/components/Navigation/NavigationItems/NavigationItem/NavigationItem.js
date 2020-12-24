01b53f6cbb9c4d72bd526836a6f7685ff6b786e4
mroot <almatov.ms@gmail.com>
2020-12-24 21:05:03 +0500

    fully implemented Orders route

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 import styles from './NavigationItem.module.css'
 
 const NavigationItem = (props) => (
-<li className={styles.NavigationItem}>
-    <a
-        href={props.link}
-        className={props.active ? styles.active : null} >{props.children}</a>
-</li>
+    <li className={styles.NavigationItem}>
+        <NavLink 
+            to={props.link}
+            exact={props.exact}
+            activeClassName={styles.active}>{props.children}</NavLink>
+    </li>
 );
 
 export default NavigationItem;
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
 import styles from './ContactData.module.css';
+import axios from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class ContactData extends Component {
     state = {
@@ -9,20 +11,59 @@ class ContactData extends Component {
         address: {
             street: '',
             postalCode: ''
-        }
+        },
+        loading: false
     };
 
+    orderHandler = (event) => {
+        event.preventDefault();
+
+        this.setState({ loading: true });
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'Murod Almatov',
+                address: {
+                    street: 'test street 1',
+                    zipCode: '123412341234',
+                    country: 'Uzbekistan'
+                },
+                email: 'almatov.ms@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+        };
+
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({ loading: false });
+                this.props.history.push('/');
+            })
+            .catch(error => {
+                this.setState({ loading: false });
+                console.log(error);
+            });
+    }
+
     render() {
+        let form = (
+            <form>
+                <input className={styles.Input} type='text' name='name' placeholder='Your Name' />
+                <input className={styles.Input} type='text' name='email' placeholder='Your Email' />
+                <input className={styles.Input} type='text' name='street' placeholder='Street' />
+                <input className={styles.Input} type='text' name='postal' placeholder='Postal Code' />
+                <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
+            </form>
+        );
+
+        if (this.state.loading) {
+            form = <Spinner />;
+        }
+
         return (
             <div className={styles.ContactData}>
                 <h4>Enter your Contact Data</h4>
-                <form>
-                    <input className={styles.Input} type='text' name='name' placeholder='Your Name' />
-                    <input className={styles.Input} type='text' name='email' placeholder='Your Email' />
-                    <input className={styles.Input} type='text' name='street' placeholder='Street' />
-                    <input className={styles.Input} type='text' name='postal' placeholder='Postal Code' />
-                    <Button btnType='Success'>ORDER</Button>
-                </form>
+                {form}
             </div>
         );
     }

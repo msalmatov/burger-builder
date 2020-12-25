520a01d613025f2426e1c08891b00c9c3ae8f467
mroot <almatov.ms@gmail.com>
2020-12-25 16:07:06 +0500

    form inputs improved

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styles from './Input.module.css';
+
+const Input = (props) => {
+    let inputElement = null;
+    switch (props.elementType) {
+        case ('input'):
+            inputElement = <input
+                className={styles.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onClick={props.changed} />;
+            break;
+        case ('textarea'):
+            inputElement = <textarea
+                className={styles.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onClick={props.changed} />;
+            break;
+        case ('select'):
+            inputElement = (<select 
+                className={styles.InputElement}
+                value={props.value} >
+                {props.elementConfig.options.map(option => (
+                    <option key={option.key} value={option.value}
+                    onClick={props.changed} >
+                        {option.displayValue}
+                    </option>
+                ))}
+            </select>)
+            break;
+        default:
+            inputElement = <input
+                className={styles.InputElement}
+                {...props.elementConfig}
+                value={props.value} 
+                onClick={props.changed} />;
+    }
+
+    return (
+        <div className={styles.Input}>
+            <label className={styles.Label}>{props.label}</label>
+            {inputElement}
+        </div>
+    );
+};
+
+export default Input;

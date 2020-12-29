0b16fd95455c5b9f32642d1d33d2ce5ac02fa123
mroot <almatov.ms@gmail.com>
2020-12-29 16:49:23 +0500

    login/logout/redirect

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,8 +5,10 @@ import styles from './NavigationItems.module.css';
 const NavigationItems = (props) => (
     <ul className={styles.NavigationItems}>
         <NavigationItem link='/' exact >Burger Builder</NavigationItem>
-        <NavigationItem link='/orders'>Orders</NavigationItem>
-        <NavigationItem link='/auth'>Authenticate</NavigationItem>
+        { props.isAuthenticated ? <NavigationItem link='/orders'>Orders</NavigationItem> : null }
+        { !props.isAuthenticated
+            ? <NavigationItem link='/auth'>Authenticate</NavigationItem>
+            : <NavigationItem link='/logout'>Logout</NavigationItem>}
     </ul>
 );
 
